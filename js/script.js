1d47dbefd0daa29ca3b5972e8eca57930f6f5f48
Ali Kirat <91100475+peripateticlearner@users.noreply.github.com>
2024-01-12 12:10:52 -0800

    selected html elements, placeholders, guess button event listener

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 //The unordered list where the player’s guessed letters will appear.
-const guestLetters = document.querySelector(".guest-letters");
+const guessedLetters = document.querySelector(".guessed-letters");
 
 //The button with the text “Guess!” in it.
-const button = document.querySelector(".guess");
+const guessButton = document.querySelector(".guess");
 
 //The text input where the player will guess a letter.
-const guessForm = document.querySelector(".guess-form");
+const inputLetter = document.querySelector(".letter");
 
 //The empty paragraph where the word in progress will appear.
 const wordInProgress = document.querySelector(".word-in-progress");
@@ -14,13 +14,31 @@ const wordInProgress = document.querySelector(".word-in-progress");
 const remainingGuesses = document.querySelector(".remaining");
 
 //The span inside the paragraph where the remaining guesses will display.
-const guessesRemaining = document.querySelector(".remaining span");
+const remainingGuessesSpan = document.querySelector(".remaining span");
 
 //The empty paragraph where messages will appear when the player guesses a letter.
 const message = document.querySelector(".message");
 
 //The hidden button that will appear prompting the player to play again.
-const playAgain = document.querySelector(".play-again");
+const playAgainButton = document.querySelector(".play-again");
 
 const word = "magnolia";
 
+//function to add placeholder for each letter
+const placeholder = function (word) {
+    const placeholderLetters = [];
+    for (const letter of word) {
+        console.log(letter);
+        placeholderLetters.push("●");
+    };
+    wordInProgress.innerText = placeholderLetters.join("");
+};
+placeholder(word);
+
+//Event listener for the button
+guessButton = addEventListener ("click", function(e) {
+    e.preventDefault();
+    const guess = inputLetter.value;
+    console.log(guess);
+    inputLetter.value = "";
+});
