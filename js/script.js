ca4346d6cb949e1e3e9ec23b0a0fa96688d06179
Ali Kirat <91100475+peripateticlearner@users.noreply.github.com>
2024-03-07 08:47:34 -0800

    display word and guessed letter
    Created a function to show the guessed letters, created a function to update the word in progress, and created a function to check if the player won.

--- js/script.js
+++ js/script.js
@@ -73,5 +73,41 @@ const makeGuess = function (guess) {
     } else {
         guessedLetters.push(guess);
         console.log(guessedLetters);
+        showGuess();
+        updateWordInProgress(guessedLetters);
+    }
+};
+
+// function displays the guessed letters
+const showGuess = function () {
+    guessedLettersElement.innerHTML = "";
+    for (const letter of guessedLetters) {
+        const li = document.createElement("li");
+        li.innerText = letter;
+        guessedLettersElement.append(li);
+    }
+};
+
+// function update word in progress
+const updateWordInProgress = function (guessedLetters) {
+    const wordUpper = word.toUpperCase();
+    const wordArray = wordUpper.split("");
+    const showWord = [];
+    for (const letter of wordArray) {
+      if (guessedLetters.includes(letter)) {
+        showWord.push(letter.toUpperCase());
+    } else {
+        showWord.push("●");
+    }
+    console.log(showWord);
+    }
+    wordInProgress.innerText = showWord.join("");
+    CheckForWin();
+};
+
+const CheckForWin = function () {
+    if (word.toUpperCase() === wordInProgress.innerText) {
+        message.classList.add("win");
+        message.innerHTML = `<p class="highlight">You guessed correct the word! Congrats!</p>`;
     }
 };
